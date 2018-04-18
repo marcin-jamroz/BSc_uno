6770f7a07395534188c79aff1f1441f12f9c257d
Marcin <marcin919@op.pl>
2018-04-18 20:07:11 +0200

    initial commit

---
+++ nanoRPC_iface.ino
@@ -0,0 +1,204 @@
+/* BSD 2-Clause License
+
+Copyright (c) 2018, Marcin Jamroz
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+#ifndef __NANORPC_IFACE__
+#define __NANORPC_IFACE__
+
+#define SYN_MSG   0x00
+#define SEND_MSG  0x01
+#define ACK_MSG   0x02
+
+#define POUART_HEADER_SIZE  2
+#define POUART_RELIABLE_HEADER_SIZE  (POUART_HEADER_SIZE+1)
+
+#define ACK_POUART_CHANNEL 10
+#define MSG_MAX_BUFFER 50
+
+const unsigned long reSendTimer = 100;
+uint8_t max_resend = 5;
+unsigned long sendTime = 0;
+byte resendCounter = 0;
+uint8_t msg_copy[MSG_MAX_BUFFER];
+uint8_t msg_copy_size = 0;
+
+uint8_t seqCur = 0;
+uint8_t seqPrev = 0;
+
+boolean isAck = true;
+
+
+void receive()
+{
+  RF24NetworkHeader header;
+  uint8_t PoUARTframe[MSG_MAX_BUFFER];
+  uint8_t frameSize = network.read(header, &PoUARTframe, MSG_MAX_BUFFER);
+
+  uint8_t tmpType = 0;
+  uint8_t tmpSeq = 0;
+
+  tmpType = PoUARTframe[0] & 0x03;
+  tmpSeq = PoUARTframe[0] >> 2;
+  
+  uint8_t channelID = PoUARTframe[1];
+
+  if (tmpType == SEND_MSG && seqPrev != tmpSeq) {
+    seqPrev = tmpSeq;
+    sendPoUART_ACK(ACK_POUART_CHANNEL, 0, NULL);
+    isAck = false;
+
+    uint8_t len = PoUARTframe[2];
+    uint8_t payload[len];
+
+    for (int i = 0; i < len; i++) {
+      payload[i] = PoUARTframe[i + 3];
+    }
+
+    recvPoUART(channelID, len, payload);
+
+    //    Serial.print(F("ChannelID=")); Serial.println(channelID);
+    //    Serial.print(F("Len=")); Serial.println(len);
+    //    Serial.print(F("Choice=")); Serial.println(choice);
+    //    Serial.print(F("MoveSpeed=")); Serial.println(moveSpeed);
+    //    Serial.print(F("MoveTime=")); Serial.println(moveTime);
+
+  } else if (tmpType == ACK_MSG && tmpSeq == seqCur && channelID == ACK_POUART_CHANNEL) {
+    Serial.println("Otrzymalem potwierdzenie");
+    isAck = true;
+    //   Serial.println("Dostalem potwierdzenie od UNO");
+  } else if (seqPrev == tmpSeq) {
+    sendPoUART_ACK(10, 0, NULL);
+  }
+}
+
+uint8_t inc(uint8_t tmp) {
+  tmp++;
+  if (tmp > 63) {
+    tmp = 0;
+  }
+  return tmp;
+}
+
+
+boolean resend_bg() {
+  if (millis() - sendTime > reSendTimer && isAck == false && resendCounter < max_resend) {
+    uint8_t tmp_msg[msg_copy_size];
+    memcpy(&tmp_msg, &msg_copy, msg_copy_size);
+    RF24NetworkHeader header(remote_node);
+    sendTime = millis();
+    network.write(header, &tmp_msg, sizeof(tmp_msg));
+    resendCounter++;
+    return true;
+    Serial.print("Wysylam resend nr: "); Serial.println(resendCounter);
+  } else if(resendCounter == max_resend && isAck == false){
+    isAck = true;
+    return false;
+  } else {
+    return true;
+  }
+}
+
+void sendPoUART_ACK(uint8_t channel, uint8_t payload_len, uint8_t *payload) {
+  uint8_t PoUARTframe[POUART_RELIABLE_HEADER_SIZE + payload_len];
+
+  PoUARTframe[0] = (uint8_t)(ACK_MSG);
+  PoUARTframe[0] = (PoUARTframe[0] & 0x03) | (seqPrev << 2);
+
+  for (int i = 0; i < payload_len; i++) {
+    PoUARTframe[i + POUART_RELIABLE_HEADER_SIZE] = payload[i];
+  }
+
+  PoUARTframe[1] = channel;
+  PoUARTframe[2] = payload_len;
+
+  RF24NetworkHeader header(remote_node);
+  network.write(header, &PoUARTframe, sizeof(PoUARTframe));
+
+  ////////////////// DEBUG /////////////////////////////
+  Serial.println("Wysylam wiadomosc ACK: ");
+  // wyslane++;
+}
+
+// //Communication support in external functions:
+void sendPoUART(uint8_t channel, uint8_t payload_len, uint8_t *payload) {
+
+  uint8_t PoUARTframe[POUART_RELIABLE_HEADER_SIZE + payload_len];
+
+  seqCur = inc(seqCur);
+  PoUARTframe[0] = (uint8_t)(SEND_MSG);
+  PoUARTframe[0] = (PoUARTframe[0] & 0x03) | (seqCur << 2);
+
+  for (int i = 0; i < payload_len; i++) {
+    PoUARTframe[i + POUART_RELIABLE_HEADER_SIZE] = payload[i];
+  }
+  sendTime = millis();
+
+  PoUARTframe[1] = channel;
+  PoUARTframe[2] = payload_len;
+
+  memcpy(&msg_copy, &PoUARTframe, sizeof(PoUARTframe));
+  msg_copy_size = sizeof(PoUARTframe);
+
+  RF24NetworkHeader header(remote_node);
+  network.write(header, &PoUARTframe, sizeof(PoUARTframe));
+
+  isAck = false;
+  resendCounter = 0;
+
+  /////////////////// DEBUG /////////////////////
+  // Serial.println("Wysylam wiadomosc do MINI: ");
+  // Serial.print("seqCur: "); Serial.println(seqCur);
+  // Serial.print("HEX ramka: "); Serial.println(PoUARTframe[0], HEX);
+  // Serial.print("BIN ramka: "); Serial.println(PoUARTframe[0], BIN);
+  // wyslane++;
+}
+
+// Error support in external functions:
+void NANORPC_FRAME_ERROR(uint8_t error_val) {
+
+};
+
+//Big-little endiannes in support external functions:
+uint32_t MHTONL(uint32_t tmp) {
+  return tmp;
+};
+
+uint32_t MNTOHL(uint32_t tmp) {
+  return tmp;
+};
+
+uint16_t MHTONS(uint16_t tmp) {
+  return tmp;
+};
+
+uint16_t MNTOHS(uint16_t tmp) {
+  return tmp;
+};
+
+#endif //__NANORPC_IFACE__
+
+
---
+++ uno.ino
@@ -0,0 +1,267 @@
+/* BSD 2-Clause License
+
+Copyright (c) 2018, Marcin Jamroz
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+
+#include <RF24Network.h>
+#include <RF24.h>
+#include <SPI.h>
+#include <Ethernet.h>
+#include <PubSubClient.h>
+#include "uno.h"
+
+// nrf24L01+ configuration
+RF24 radio(A0, A1);
+RF24Network network(radio);
+
+const uint16_t this_node = 00;
+const uint16_t remote_node = 01;
+
+
+// Ethernet configuration
+byte mac[]    = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
+//OLEK IPAddress my_ip(192, 168, 0, 200);
+
+EthernetClient ethernetClient;
+//end
+
+
+// MQTT configuration
+IPAddress mqtt_server(192, 168, 0, 105);
+
+PubSubClient mqttClient(ethernetClient);
+// end
+
+
+
+
+//debug
+int odebrane = 0;
+int wyslane = 0;
+unsigned long debugTime = 0;
+unsigned long debugSendTime = 0;
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Uno started");
+
+  radio.begin();
+  network.begin(90, this_node);
+
+  mqttClient.setServer(mqtt_server, 1883);
+  mqttClient.setCallback(callback);
+
+  //OLEK Ethernet.begin(mac, my_ip);
+  Ethernet.begin(mac);
+  Serial.print(F("My IP address: "));
+  for (byte thisByte = 0; thisByte < 4; thisByte++) {
+    Serial.print(Ethernet.localIP()[thisByte], DEC);
+    Serial.print(F("."));
+  }
+  Serial.println();
+// radio.setAutoAck(false);
+
+
+  Serial.println("Uno started");
+
+  delay(1500);
+}
+
+void loop() {
+
+//  if (millis() - debugSendTime > 500 && wyslane < 500) {
+//    sendPoUART(10, 0, NULL);
+//    wyslane++;
+//    debugSendTime = millis();
+//  }
+
+//  if (millis() - debugTime > 10000) {
+//    //Serial.print("--------wyslano: "); Serial.println(wyslane);
+//    Serial.print("--------odebrane: "); Serial.println(odebrane);
+//    debugTime = millis();
+//  }
+
+  network.update();
+
+  while (network.available()) {
+    receive();
+  }
+
+  if (resend_bg() == false){
+        mqttClient.publish(ERROR_TOPIC, NO_SEND_MINI, sizeof(NO_SEND_MINI));
+  }
+
+  if (!mqttClient.connected()) {
+    reconnect();
+  }
+  mqttClient.loop();
+
+}
+
+int convertToInt(int byteRead)
+{
+  return byteRead - '0';
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  {
+    char choice = 'a';
+    uint16_t moveTime = 0;
+    uint8_t moveSpeed = 0;
+
+    //   Serial.print(F("Message arrived ["));
+    //  Serial.print(topic);
+    //  Serial.println(F("] "));
+
+    choice = (char)payload[0];
+
+    if (choice == UP || choice == DOWN || choice == LEFT || choice == RIGHT) {
+      int i = 1;
+      char tmp = (char)payload[1];
+
+      while (tmp != ',') {
+        moveTime = moveTime * 10 + convertToInt(payload[i]);
+        i++;
+        tmp = (char)payload[i];
+      }
+
+      i++;
+
+      while (i < length) {
+        moveSpeed = moveSpeed * 10 + convertToInt(payload[i]);
+        i++;
+      }
+    }
+
+    if (choice == UP) forward(moveTime, moveSpeed);
+    else if (choice == DOWN) backward(moveTime, moveSpeed);
+    else if (choice == RIGHT) right(moveTime, moveSpeed);
+    else if (choice == LEFT) left(moveTime, moveSpeed);
+    else if (choice == CHECK_PROGRESS) checkProgress();
+    else if (choice == STOP) stop();
+  }
+}
+
+
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!mqttClient.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (mqttClient.connect("arduinoGateway")) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      mqttClient.subscribe(SEND_TOPIC);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(mqttClient.state());
+      Serial.println(" try again in 5 seconds");
+      switch (mqttClient.state()) {
+        case MQTT_CONNECTION_TIMEOUT:
+          Serial.println(F("Error: MQTT_CONNECTION_TIMEOUT!!!"));
+          break;
+        case MQTT_CONNECTION_LOST:
+          Serial.println(F("Error: MQTT_CONNECTION_LOST!!!"));
+          break;
+        case MQTT_CONNECT_FAILED:
+          Serial.println(F("Error: MQTT_CONNECT_FAILED!!!"));
+          break;
+        case MQTT_DISCONNECTED:
+          Serial.println(F("Error: MQTT_DISCONNECTED!!!"));
+          break;
+        case MQTT_CONNECTED://???
+        case MQTT_CONNECT_BAD_PROTOCOL:
+          Serial.println(F("Error: MQTT_CONNECT_BAD_PROTOCOL!!!"));
+          break;
+        case MQTT_CONNECT_BAD_CLIENT_ID:
+          Serial.println(F("Error: MQTT_CONNECT_BAD_CLIENT_ID!!!"));
+          break;
+        case MQTT_CONNECT_UNAVAILABLE:
+          Serial.println(F("Error: MQTT_CONNECT_UNAVAILABLE!!!"));
+          break;
+        case MQTT_CONNECT_BAD_CREDENTIALS:
+          Serial.println(F("Error: MQTT_CONNECT_BAD_CREDENTIALS!!!"));
+          break;
+        case MQTT_CONNECT_UNAUTHORIZED:
+          Serial.println(F("Error: MQTT_CONNECT_UNAUTHORIZED!!!"));
+          break;
+        default:
+          Serial.println(F("Error: UNKNOWN ("));
+          Serial.print(mqttClient.state()); Serial.println(F(")"));
+      }
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+// Methods to handle returns from mini pro
+void forwardSrvRet(uint8_t a) {
+  if (a == 1) {
+    mqttClient.publish(ANSWER_TOPIC, COMM, sizeof(COMM));
+  //  Serial.println(F("WysĹ‚ano zlecenie forward"));
+  }
+  else
+    mqttClient.publish(ERROR_TOPIC, NOCOMM, sizeof(NOCOMM));
+}
+void backwardSrvRet(uint8_t a) {
+  if (a == 1) {
+    mqttClient.publish(ANSWER_TOPIC, COMM, sizeof(COMM));
+    Serial.println(F("WysĹ‚ano zlecenie backward"));
+  } else
+    mqttClient.publish(ERROR_TOPIC, NOCOMM, sizeof(NOCOMM));
+}
+void leftSrvRet(uint8_t a) {
+  if (a == 1) {
+    mqttClient.publish(ANSWER_TOPIC, COMM, sizeof(COMM));
+    Serial.println(F("WysĹ‚ano zlecenie left"));
+  } else
+    mqttClient.publish(ERROR_TOPIC, NOCOMM, sizeof(NOCOMM));
+}
+void rightSrvRet(uint8_t a) {
+  if (a == 1) {
+    mqttClient.publish(ANSWER_TOPIC, COMM, sizeof(COMM));
+    Serial.println(F("WysĹ‚ano zlecenie right"));
+  } else
+    mqttClient.publish(ERROR_TOPIC, NOCOMM, sizeof(NOCOMM));
+}
+void checkProgressSrvRet(uint16_t a) {
+  char tmp[sizeof(uint16_t)];
+  dtostrf(a, sizeof(uint16_t), 0, tmp);
+
+  mqttClient.publish(ANSWER_TOPIC, tmp, sizeof(tmp));
+  Serial.println(F("WysĹ‚ano zlecenie check"));
+}
+void stopSrvRet(uint8_t a) {
+  if (a == 1) {
+    mqttClient.publish(ANSWER_TOPIC, COMM, sizeof(COMM));
+    Serial.println(F("WysĹ‚ano zlecenie stop"));
+  } else
+    mqttClient.publish(ERROR_TOPIC, NOCOMM, sizeof(NOCOMM));
+}
+
